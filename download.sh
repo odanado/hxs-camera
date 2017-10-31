3d82df9ff7dff3ed9a640873340650784cfcd15d
kita <e1b14036@st.oit.ac.jp>
2017-11-01 04:42:19 +0900

    fix download script

--- download.sh
+++ download.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 mkdir -p weights
-wget https://pjreddie.com/media/files/tiny-yolo-voc.weights -P weights
+if [ ! -e weights/tiny-yolo-voc.weights ]; then
+    wget https://pjreddie.com/media/files/tiny-yolo-voc.weights -P weights
+fi
+
+mkdir -p cfg
+if [ ! -e cfg/tiny-yolo-voc.cfg ]; then
+    wget https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/tiny-yolo-voc.cfg -P cfg
+fi
+
+mkdir -p data
+if [ ! -e data/dog.jpg ]; then
+    wget https://github.com/pjreddie/darknet/raw/master/data/dog.jpg -P data
+fi
